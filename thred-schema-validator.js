fde8f95c37b2e9e038f73d1d40d5ee319255a82f
Keiran Stokes <keiran@thred.co.nz>
2022-06-22 11:30:48 +1200

    fix issue #1

--- thred-schema-validator.js
+++ thred-schema-validator.js
@@ -12,41 +12,46 @@ module.exports = function(RED) {
         const Ajv2020 = require("ajv/dist/2020");
         const ajv = new Ajv2020({loadSchema: loadSchema});
 
+        const cache = new Map();
+
 
         async function loadSchema(url) {
             try {
               const response = await axios.get(url);
               return response.data
             } catch (error) {
-                throw Error(error);
-            }
-
+                throw Error(err)
+              }
           }
-        
 
         this.on('input', function(msg, send, done) {
 
             async function validate(schema_id, payload) {
               let validate_obj;
+              let promise;
               let valid;
               let errors;
               let schema;
-              // check if valid
-              let is_valid = ajv.getSchema(msg.schema_id);
-              // if valid, validate
+
+              let is_valid = await ajv.getSchema(schema_id);
+              
               if (is_valid) {
                 validate_obj = is_valid;
               } else {
-                // fetch schema
                 try {
-                  schema = await loadSchema(schema_id);
+                  const existingPromise = cache.get(schema_id);
+                  if (existingPromise) {
+                    promise = existingPromise;
+                  } else {
+                    promise = loadSchema(schema_id);
+                    cache.set(schema_id, promise);
+                  }
+                  schema = await promise;
+                  validate_obj = await ajv.compileAsync(schema);
                 } catch (err) {
-                  throw Error(err)
+                    throw Error(err)
+                }
               }
-                // validate
-                validate_obj = await ajv.compileAsync(schema);
-              }
-              // return
               valid = validate_obj(payload);
               errors = validate_obj.errors;
               return [valid, errors]
@@ -59,10 +64,6 @@ module.exports = function(RED) {
                 if (r[1] !== null) {
                   msg.payload.$errors = r[1];
                 }
-                // msg.validation = {
-                //   "valid": r[0],
-                //   "errors": r[1]
-                // };
                 send(msg);
               })
               .catch(function (err) {
